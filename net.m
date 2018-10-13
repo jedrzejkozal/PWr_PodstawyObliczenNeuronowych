fe2d44316d7cdaf8221c35c529ee53b7e0e24d9b
jedrzejkozal <jedrzej.kozal@gmail.com>
2018-10-14 00:02:57 +0200

    changed test set to 24 first elements without sorting

--- net.m
+++ net.m
@@ -1,30 +1,30 @@
-samples = read_data();
+[samples_traning, samples_test] = read_data();
 
 motor_UPDRS_index = 1;
 total_UPDRS_index = 2;
 test_time_index = 3;
 
 number_of_patients = 42;
-traning_set_size = 30;
-test_set_size = 12;
+traning_set_size = size(samples_traning, 2);
+test_set_size = size(samples_test, 2);
 
 input = [];
 target = [];
 for i = 1:traning_set_size
-    input = [input [samples{i}(:, 3:5)'; samples{i}(:, 6)'; samples{i}(:, 12)'] ]; 
-    target = [target samples{i}(:, motor_UPDRS_index)'];
+    input = [input [samples_traning{i}(:, 4:5)'; samples_traning{i}(:, 6)'; samples_traning{i}(:, 12)'] ]; 
+    target = [target samples_traning{i}(:, motor_UPDRS_index)'];
 end
 
-eg = 0.05; % sum-squared error goal
-sc = 1;   % spread constant
+eg = 8.0; % sum-squared error goal
+sc = 0.1;   % spread constant
 RBF_net = newrb(input,target,eg,sc);
 
 %validation with traning set
 figure(1)
 for j = 1:traning_set_size
-    time = samples{j}(:, test_time_index)';
-    input1 = [samples{j}(:, 3:5)'; samples{j}(:, 6)'; samples{j}(:, 12)'];
-    UPDRS1 = samples{j}(:, motor_UPDRS_index)';
+    time = samples_traning{j}(:, test_time_index)';
+    input1 = [samples_traning{j}(:, 4:5)'; samples_traning{j}(:, 6)'; samples_traning{j}(:, 12)'];
+    UPDRS1 = samples_traning{j}(:, motor_UPDRS_index)';
 
     subplot(5,6,j);
     output = RBF_net(input1);
@@ -37,11 +37,9 @@ end
 %validation with test set
 figure(2)
 for k = 1:test_set_size
-    vector_from_test_set_index = traning_set_size+k;
-    
-    time = samples{vector_from_test_set_index}(:, test_time_index);
-    input2 = [samples{vector_from_test_set_index}(:, 3:5)'; samples{vector_from_test_set_index}(:, 6)'; samples{vector_from_test_set_index}(:, 12)';];
-    UPDRS2 = samples{vector_from_test_set_index}(:, motor_UPDRS_index)';
+    time = samples_test{k}(:, test_time_index);
+    input2 = [samples_test{k}(:, 4:5)'; samples_test{k}(:, 6)'; samples_test{k}(:, 12)';];
+    UPDRS2 = samples_test{k}(:, motor_UPDRS_index)';
 
     subplot(3,4,k);
     output2 = RBF_net(input2);
--- read_data.m
+++ read_data.m
@@ -1,4 +1,4 @@
-function samples = read_data()
+function [samples_traning, samples_test] = read_data()
 
 data = importdata("parkinson.data");
 
@@ -6,15 +6,29 @@ number_of_patients = 42;
 
 samples = cell(1, number_of_patients);
 
+%crop out the patint_id
 for i = 1:size(data,1)
     patient_id = data(i,1);
     samples{patient_id} = [samples{patient_id}; data(i,2:end)];
 end
 
+%swap age and sex with motor_UPDRS,total_UPDRS cloumns
 for i = 1:size(samples, 2)
    samples{i}(:,[1 4]) = samples{i}(:,[4 1]);
    samples{i}(:,[2 5]) = samples{i}(:,[5 2]);
-   
-   samples{i} = sortrows(samples{i}, 3);
 end
 
+samples_test = cell(1, number_of_patients);
+for i = 1:size(samples,2)
+    samples_test{i} = samples{i}(1:24, :);
+    samples_traning{i} = samples{i}(24:end, :);
+end
+
+%sort through time
+for i = 1:size(samples_test, 2)
+   samples_test{i} = sortrows(samples_test{i}, 3);
+end
+
+for i = 1:size(samples_traning, 2)
+    samples_traning{i} = sortrows(samples_traning{i}, 3);
+end
