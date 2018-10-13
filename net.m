40344b23e47868cba0e6c3d0be7b89b5046fe89e
jedrzejkozal <jedrzej.kozal@gmail.com>
2018-10-13 18:13:44 +0200

    refactor

--- net.m
+++ net.m
@@ -1,10 +1,8 @@
 samples = read_data();
 
-age_index = 1;
-sex_index = 2;
+motor_UPDRS_index = 1;
+total_UPDRS_index = 2;
 test_time_index = 3;
-motor_UPDRS_index = 4;
-total_UPDRS_index = 5;
 
 number_of_patients = 42;
 traning_set_size = 30;
@@ -13,41 +11,25 @@ test_set_size = 12;
 input = [];
 target = [];
 for i = 1:traning_set_size
-    age = samples{i}(:, age_index);
-    sex = samples{i}(:, sex_index);
-    time = samples{i}(:, test_time_index);
-    UPDRS = samples{i}(:, motor_UPDRS_index);
-
-    merged = [time'; UPDRS'];
-    sorted = sortrows(merged', 1)';
-    time = sorted(1, :);
-    UPDRS = sorted(2, :);
-    input = [input [age'; sex'; time]];
-    target = [target UPDRS];
+    input = [input samples{i}(:, 3:end)'];
+    target = [target samples{i}(:, motor_UPDRS_index)'];
 end
 
-eg = 0.05; % sum-squared error goal
+eg = 0.1; % sum-squared error goal
 sc = 1;    % spread constant
-%RBF_net = newrb(input,target,eg,sc);
+RBF_net = newrb(input,target,eg,sc);
 
 %validation with traning set
 figure(1)
 for j = 1:traning_set_size
-    vector_from_test_set_index = j;
-    age = samples{vector_from_test_set_index}(:, age_index);
-    sex = samples{vector_from_test_set_index}(:, sex_index);
-    time = samples{vector_from_test_set_index}(:, test_time_index);
-    UPDRS = samples{vector_from_test_set_index}(:, motor_UPDRS_index);
-
-    merged = [time'; UPDRS'];
-    sorted = sortrows(merged', 1)';
-    time = sorted(1, :);
-    UPDRS = sorted(2, :);
+    time = samples{j}(:, test_time_index)';
+    input1 = samples{j}(:, 3:end)';
+    UPDRS1 = samples{j}(:, motor_UPDRS_index)';
 
     subplot(5,6,j);
-    output = RBF_net([age'; sex'; time]);
+    output = RBF_net(input1);
     hold on;
-    plot(time, UPDRS, 'bo');
+    plot(time, UPDRS1, 'bo');
     plot(time, output,'r-');
     %legend({'Target','Output'})
 end
@@ -56,20 +38,15 @@ end
 figure(2)
 for k = 1:test_set_size
     vector_from_test_set_index = traning_set_size+k;
-    age = samples{vector_from_test_set_index}(:, age_index);
-    sex = samples{vector_from_test_set_index}(:, sex_index);
+    
     time = samples{vector_from_test_set_index}(:, test_time_index);
-    UPDRS = samples{vector_from_test_set_index}(:, motor_UPDRS_index);
-
-    merged = [time'; UPDRS'];
-    sorted = sortrows(merged', 1)';
-    time = sorted(1, :);
-    UPDRS = sorted(2, :);
+    input2 = samples{vector_from_test_set_index}(:, 3:end)';
+    UPDRS2 = samples{vector_from_test_set_index}(:, motor_UPDRS_index)';
 
     subplot(3,4,k);
-    output = RBF_net([age'; sex'; time]);
+    output2 = RBF_net(input2);
     hold on;
-    plot(time, UPDRS, 'bo');
-    plot(time, output,'r-');
-    legend({'Target','Output'})
+    plot(time, UPDRS2, 'bo');
+    plot(time, output2,'r-');
+    %legend({'Target','Output'})
 end
--- read_data.m
+++ read_data.m
@@ -10,3 +10,11 @@ for i = 1:size(data,1)
     patient_id = data(i,1);
     samples{patient_id} = [samples{patient_id}; data(i,2:end)];
 end
+
+for i = 1:size(samples, 2)
+   samples{i}(:,[1 4]) = samples{i}(:,[4 1]);
+   samples{i}(:,[2 5]) = samples{i}(:,[5 2]);
+   
+   samples{i} = sortrows(samples{i}, 3);
+end
+
