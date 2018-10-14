216683b0c07d3546ed6b00e3cbd34edc5470f000
filipmg <filip.m.guzy@gmail.com>
2018-10-14 14:13:40 +0200

    For one patient

--- net.m
+++ net.m
@@ -5,28 +5,28 @@ total_UPDRS_index = 2;
 test_time_index = 3;
 
 number_of_patients = 42;
-training_set_size = size(samples_training, 2);
-test_set_size = size(samples_test, 2);
+training_set_size = 1;%size(samples_training, 2);
+test_set_size = 1;%size(samples_test, 2);
 
 input = [];
 target = [];
 for i = 1:training_set_size
-    input = [input [samples_training{i}(:, 4:5)'; samples_training{i}(:, 6)'; samples_training{i}(:, 12)'] ]; 
+    input = [input samples_training{i}(:, 3:21)']; 
     target = [target samples_training{i}(:, motor_UPDRS_index)'];
 end
 
-eg = 8.0; % sum-squared error goal 
-sc = 0.1; % spread constant
+eg = 0.01e-09; % sum-squared error goal 
+sc = 10; % spread constant
 RBF_net = newrb(input,target,eg,sc);
 
 %validation with traning set
 figure(1)
 for j = 1:training_set_size
     time = samples_training{j}(:, test_time_index)';
-    input1 = [samples_training{j}(:, 4:5)'; samples_training{j}(:, 6)'; samples_training{j}(:, 12)'];
+    input1 = samples_training{i}(:, 3:21)';
     UPDRS1 = samples_training{j}(:, motor_UPDRS_index)';
 
-    subplot(6,7,j);
+    %subplot(6,7,j);
     output = RBF_net(input1);
     hold on;
     plot(time, UPDRS1, 'bo');
@@ -38,10 +38,10 @@ end
 figure(2)
 for k = 1:test_set_size
     time = samples_test{k}(:, test_time_index);
-    input2 = [samples_test{k}(:, 4:5)'; samples_test{k}(:, 6)'; samples_test{k}(:, 12)'];
+    input2 = samples_test{k}(:, 3:21)';
     UPDRS2 = samples_test{k}(:, motor_UPDRS_index)';
 
-    subplot(6,7,k);
+    %subplot(6,7,k);
     output2 = RBF_net(input2);
     hold on;
     plot(time, UPDRS2, 'bo');
