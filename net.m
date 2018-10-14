c7eba800e50faf6a87f16fadf53b2652fc085957
jedrzejkozal <jedrzej.kozal@gmail.com>
2018-10-14 18:41:11 +0200

    merge fix

--- net.m
+++ net.m
@@ -8,8 +8,8 @@ total_UPDRS_index = 2;
 test_time_index = 3;
 
 number_of_patients = 42;
-training_set_size =  size(samples_training, 2)
-test_set_size =  size(samples_test, 2)
+training_set_size =  1;%size(samples_training, 2)
+test_set_size =  1;%size(samples_test, 2)
 
 input = [];
 target = [];
@@ -18,8 +18,8 @@ for i = 1:training_set_size
     target = [target samples_training{i}(:, motor_UPDRS_index)'];
 end
 
-eg = 0.1 % sum-squared error goal 
-sc = 0.1 % spread constant
+eg = 0.01e-09; % sum-squared error goal 
+sc = 10; % spread constant
 RBF_net = newrb(input,target,eg,sc);
 
 %validation with traning set
@@ -29,7 +29,7 @@ for j = 1:training_set_size
     input1 = [samples_training{j}(:, 4:5)'; samples_training{j}(:, 6)'; samples_training{j}(:, 12)']; %samples_training{j}(:, 3:21)';
     UPDRS1 = samples_training{j}(:, motor_UPDRS_index)';
 
-    subplot(6,7,j);
+    %subplot(6,7,j);
     output1 = RBF_net(input1);
     hold on;
     plot(time1, UPDRS1, 'bo');
@@ -45,7 +45,7 @@ for k = 1:test_set_size
     input2 = [samples_test{k}(:, 4:5)'; samples_test{k}(:, 6)'; samples_test{k}(:, 12)']; %samples_test{k}(:, 3:21)';
     UPDRS2 = samples_test{k}(:, motor_UPDRS_index)';
 
-    subplot(6,7,k);
+    %subplot(6,7,k);
     output2 = RBF_net(input2);
     hold on;
     plot(time2, UPDRS2, 'bo');
