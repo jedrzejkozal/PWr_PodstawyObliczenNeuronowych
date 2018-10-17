f11daac48b9ee44685c1f480d445ec66195bc8ad
filipmg <filip.m.guzy@gmail.com>
2018-10-17 20:15:58 +0200

    Normalizacja

--- net.m
+++ net.m
@@ -8,8 +8,13 @@ total_UPDRS_index = 2;
 test_time_index = 3;
 
 number_of_patients = 42;
-training_set_size =  1;%size(samples_training, 2)
-test_set_size =  1;%size(samples_test, 2)
+training_set_size =  42;%size(samples_training, 2)
+test_set_size =  42;%size(samples_test, 2)
+
+for i = 1:number_of_patients
+   samples_training{i}(:,[3 18]) = normalize(samples_training{i}(:, [3 18]), 'range');
+   samples_test{i}(:,[3 18]) = normalize(samples_test{i}(:,[3 18]), 'range');
+end
 
 input = [];
 target = [];
@@ -19,7 +24,7 @@ for i = 1:training_set_size
 end
 
 eg = 0.01e-09; % sum-squared error goal 
-sc = 10; % spread constant
+sc = 1; % spread constant
 RBF_net = newrb(input,target,eg,sc);
 
 %validation with traning set
@@ -29,7 +34,7 @@ for j = 1:training_set_size
     input1 = [samples_training{j}(:, 4:5)'; samples_training{j}(:, 6)'; samples_training{j}(:, 12)']; %samples_training{j}(:, 3:21)';
     UPDRS1 = samples_training{j}(:, motor_UPDRS_index)';
 
-    %subplot(6,7,j);
+    subplot(6,7,j);
     output1 = RBF_net(input1);
     hold on;
     plot(time1, UPDRS1, 'bo');
@@ -38,20 +43,17 @@ for j = 1:training_set_size
 end
 
 %validation with test set
-MSE = 0;
 figure(2)
 for k = 1:test_set_size
     time2 = samples_test{k}(:, test_time_index);
     input2 = [samples_test{k}(:, 4:5)'; samples_test{k}(:, 6)'; samples_test{k}(:, 12)']; %samples_test{k}(:, 3:21)';
     UPDRS2 = samples_test{k}(:, motor_UPDRS_index)';
 
-    %subplot(6,7,k);
+    subplot(6,7,k);
     output2 = RBF_net(input2);
     hold on;
     plot(time2, UPDRS2, 'bo');
     plot(time2, output2,'r-');
     
-    MSE = MSE + immse(UPDRS2,output2);
     %legend({'Target','Output'})
 end
-MSE
